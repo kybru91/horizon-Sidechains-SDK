adc4370c544dd88ff50970a48a274ec82080cd3b
Alberto Sala <albertosala@horizenlabs.io>
2024-04-24 15:58:23 +0200

    Modification in comments of forger stakes solidity interface

--- qa/SidechainTestFramework/account/smart_contract_resources/contracts/ForgerStakesV2.sol
+++ qa/SidechainTestFramework/account/smart_contract_resources/contracts/ForgerStakesV2.sol
@@ -45,7 +45,8 @@ interface ForgerStakesV2 {
       rewardShare can range in [0..1000] and can be 0 if and only if rewardAddress == 0x000..00.
       Vrf key and signatures are split in two or more separate parameters, being longer than 32 bytes.
       sign1_x are the 25519 signature chunks and sign2_x are the Vfr signature chunks.
-      The message to sign is a string concatenation of signPubKey+vrfKey+rewardShare+rewardAddress
+      The message to sign is a string concatenation of signPubKey+vrfKey+rewardShare+rewardAddress, where rewardAddress
+      is represented in the Eip55 checksum format.
       The method accepts WEI value: the sent value will be converted to the initial stake assigned to the forger.
       The initial stake amount must be >= min threshold (10 Zen)
     */
