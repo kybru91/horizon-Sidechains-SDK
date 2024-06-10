600d0f27e8453f5ad4e33b5e8e300100cae6adad
Ivan Skrypnyk <vindoraga@gmail.com>
2024-06-10 13:14:20 +0300

    Add new field to zen_getFeePayments api response

--- sdk/src/main/java/io/horizen/account/api/rpc/types/FeePaymentsView.java
+++ sdk/src/main/java/io/horizen/account/api/rpc/types/FeePaymentsView.java
@@ -1,6 +1,7 @@
 package io.horizen.account.api.rpc.types;
 
 import io.horizen.account.chain.AccountFeePaymentsInfo;
+import io.horizen.account.utils.AccountPayment;
 import io.horizen.evm.Address;
 import scala.collection.JavaConverters;
 
@@ -15,17 +16,30 @@ public class FeePaymentsView {
         payments = JavaConverters
             .seqAsJavaList(info.payments())
             .stream()
-            .map(payment -> new FeePaymentData(payment.address().address(), payment.value()))
+            .map(payment -> FeePaymentData.fromAccountFeePayment(payment))
             .collect(Collectors.toList());
     }
 
     private static class FeePaymentData {
         public final Address address;
         public final BigInteger value;
+        public final BigInteger valueFromMainchain;
+        public final BigInteger valueFromFees;
 
-        public FeePaymentData(Address address, BigInteger value) {
+        public FeePaymentData(Address address, BigInteger value, BigInteger valueFromMainchain, BigInteger valueFromFees) {
             this.address = address;
             this.value = value;
+            this.valueFromMainchain = valueFromMainchain;
+            this.valueFromFees = valueFromFees;
+        }
+
+        public static FeePaymentData fromAccountFeePayment(AccountPayment payment) {
+            return new FeePaymentData(
+                payment.address().address(),
+                payment.value(),
+                payment.valueFromMainchain().getOrElse(() -> BigInteger.ZERO),
+                payment.valueFromFees().getOrElse(() -> BigInteger.ZERO)
+            );
         }
     }
 }
